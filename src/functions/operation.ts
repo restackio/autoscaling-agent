f2951f84fa5f1a59f3821176aa25519a2c08c0ee
Osman Hernandez <osman.sist@gmail.com>
2025-05-13 14:12:53 +0200

    updates to operation

--- src/functions/operation.ts
+++ src/functions/operation.ts
@@ -1,31 +1,20 @@
 import { FunctionFailure, log } from "@restackio/ai/function";
 
 const performMemoryIntensiveOperation = (): void => {
-
   const arraySize = 10000000;
   const arrays: number[][] = [];
   
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 5; i++) {
     const arr = new Array(arraySize).fill(0).map((_, index) => index);
     arrays.push(arr);
-    const arrCopy = [...arr];
-    arrays.push(arrCopy);
   }
-  
   
   for (let i = 0; i < arrays.length; i++) {
     arrays[i].sort((a, b) => b - a);
     arrays[i] = arrays[i].map(x => x * x);
     arrays[i] = arrays[i].filter(x => x % 2 === 0);
-  
-    const newArray = new Array(arraySize).fill(0).map((_, index) => arrays[i][index % arrays[i].length]);
-    arrays.push(newArray);
   }
-  
-  const result = arrays.reduce((acc, arr) => acc + arr.length, 0);
-  log.debug("Memory intensive operation completed", { result });
 };
-
 
 export const memoryIntenstiveOperation = async (): Promise<boolean> => {
   try {
